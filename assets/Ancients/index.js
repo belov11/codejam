7e6a325b7991d8544925a2994e248914525e50cb
belov11 <pasha_belov_96@mail.ru>
2022-08-28 22:14:46 +0300

    feat: add file for project

---
+++ assets/Ancients/index.js
@@ -0,0 +1,18 @@
+/* import azathoth from './Azathoth.png';
+import cthulhu from './Cthulthu.png';
+import iogSothoth from './IogSothoth.png';
+import shubNiggurath from './ShubNiggurath.png';
+ */
+let azathoth = './Azathoth.png'
+let cthulhu = './Cthulthu.png'
+let iogSothoth = './IogSothoth.png'
+let shubNiggurath = './ShubNiggurath.png'
+
+const ancients = {
+  azathoth,
+  cthulhu,
+  iogSothoth,
+  shubNiggurath
+}
+
+export default ancients
